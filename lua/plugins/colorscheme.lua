3a4b2c70a9e790a7f76f9e7e52b822eb504a1ead
ynok7 <yuugabriel727@gmail.com>
2025-06-17 03:00:23 -0300

    lazygit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,8 @@ return {
 	name = "catppuccin",
 	config = function()
 	    require("catppuccin").setup({
-		transparent_background = true,
+		flavour = "auto",
+		-- transparent_background = true,
 	    })
 	    vim.cmd.colorscheme("catppuccin")
 	end,
