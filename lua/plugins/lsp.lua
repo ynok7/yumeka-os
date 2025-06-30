2ac9d23e0ecf43cdfcb6bcf9cb0877abec8a60ab
ynok7 <yuugabriel727@gmail.com>
2025-06-30 16:28:21 -0300

    added vue_ls (volar)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,39 @@ return {
 	    cap = cap
 	})
 
+	lspconfig.ts_ls.setup({
+	    filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
+	})
+
+	-- vue language server
+	lspconfig.volar.setup({
+	    -- add filetypes for typescript, javascript and vue
+	    filetypes = { "vue" },
+	    init_options = {
+		vue = {
+		    -- disable hybrid mode
+		    hybridMode = false,
+		},
+	    },
+	})
+
+
+	-- lspconfig.ts_ls.setup({
+	--     cap = cap,
+	--     init_options = {
+	-- 	plugins = {
+	-- 	    {
+	-- 		name = "@vue/typescript-plugin",
+	-- 		location = "/usr/local/lib/node_modules/@vue/language-server",
+	-- 		languages = { "vue" },
+	-- 	    }
+	-- 	}
+	--     },
+	--     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	-- })
+
+
+	-- Keybinds
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 	vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
